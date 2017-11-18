bb5aa6acffe5cd92f48583de63f3f681cffb8146
zombieclown987 <33769622+zombieclown987@users.noreply.github.com>
2017-11-17 21:47:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'ready')
+    	message.channel.send('I Am!');
+  	}
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
