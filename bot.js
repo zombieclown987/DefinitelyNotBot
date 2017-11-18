1d0d1add0360ef69df433bdc3ce32d558f47da13
zombieclown987 <33769622+zombieclown987@users.noreply.github.com>
2017-11-17 22:00:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const music = require('discord.js-music-v11');
 const Bot = new Discord.Client();
-const token = "<auth_token>" // Recommended to load from json file.
+const token = "<MzM1MjY0NTk3ODY5MTk5Mzcx.DPEzmg.AuRs-cwhEzuY-k8G99prOsK_XWw>" // Recommended to load from json file.
 
 Bot.on('ready', () => {
     console.log(`[Start] ${new Date()}`);
